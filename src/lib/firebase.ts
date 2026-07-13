c2d83c3060497a7440e51f863cf65dc082ded0e8
Gustavo Santana <103785153+Onlyatsug@users.noreply.github.com>
2026-07-13 13:02:40 +0000

    feat: environment variable checks & .env.example

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,5 @@
+/// <reference types="vite/client" /> 
+
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
@@ -9,6 +11,14 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
+// debug: indicar a falta de alguma váriavel .env
+const required_vars = Object.entries(firebaseConfig)
+const missing_vars = required_vars.filter(([required_vars, value]) => !value).map(([key]) => key)
+
+if (missing_vars.length > 0) {
+  console.error('Missing Firebase environment variables:', missing_vars);
+}
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
