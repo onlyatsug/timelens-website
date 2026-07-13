154647174538d64faa7bddbb658a1b4d2e5f701e
Gustavo Santana <103785153+Onlyatsug@users.noreply.github.com>
2026-07-13 13:19:18 +0000

    feat: add ts configuration files and update Vite aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,25 @@
-import { defineConfig } from 'vite'
-import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-
-
-function figmaAssetResolver() {
-  return {
-    name: 'figma-asset-resolver',
-    resolveId(id) {
-      if (id.startsWith('figma:asset/')) {
-        const filename = id.replace('figma:asset/', '')
-        return path.resolve(__dirname, 'src/assets', filename)
-      }
-    },
-  }
-}
+import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
-    figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
+      '@/features/*': path.resolve(__dirname, './features/*'),
+      '@/entities/*': path.resolve(__dirname, './entities/*'),
+      '@/widgets/*': path.resolve(__dirname, './widgets/*'),
+      '@/shared/*': path.resolve(__dirname, './shared/*'),
+      '@/core/*': path.resolve(__dirname, './core/*'),
+      '@/app/*': path.resolve(__dirname, './app/*')
     },
   },
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+  // Suportar imports "crus".
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
